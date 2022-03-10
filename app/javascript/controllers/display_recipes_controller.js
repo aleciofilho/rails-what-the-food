ea5ba3d0ee920c4a3ff699ff6fe932eb1c6f080a
gabriel tavares <gabriel29t@gmail.com>
2022-03-10 18:23:26 -0300

    API request done

--- app/javascript/controllers/display_recipes_controller.js
+++ app/javascript/controllers/display_recipes_controller.js
@@ -10,12 +10,19 @@ export default class extends Controller {
   insertRecipes (data) {
     this.displayTarget.classList.remove('d-none')
     data.forEach(recipe => {
-      this.displayTarget.insertAdjacentHTML('beforeend', `<h3>${recipe['title']}</h3>`)
-      this.displayTarget.insertAdjacentHTML('beforeend', `<p>${recipe['title']}</p>`)
+      this.displayTarget.insertAdjacentHTML('beforeend',
+      `<div class="card">
+        <img src="${recipe['image']}">
+        <div class='info'>
+          <h3>${recipe['title']}</h3>
+          <p>${recipe['likes']} likes</p>
+        </div>
+      </div>`)
     });
   }
 
   fetchInput(UserInput) {
+    console.log(UserInput);
     const url = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=ad1c142c27cb4f82a8078acc4e1522d5&ingredients=${UserInput}&number=10`;
     fetch(url)
       .then(response => response.json())
@@ -27,7 +34,8 @@ export default class extends Controller {
 
   listRecipes(event) {
     event.preventDefault()
+    this.displayTarget.innerHTML = ''
     this.displayTarget.classList.add('d-none')
-    this.fetchInput(this.inputTarget.value)
+    this.fetchInput(this.inputTarget.value.replaceAll(/[, ]+/g, ',+'))
   }
 }
