70ae30773f29ea9818e9d8ee600b64619fab6e12
aleciofilho <alecio_filho@hotmail.com>
2022-03-13 19:57:19 -0300

    stimulus logic almost done. query is correct, missing request.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
     @ingredients = Ingredient.search_by_name(params[:query]) if params[:query].present?
+    @recipes = @spoonacular.recipes(params[:ingredients].sub(/[, ]+/, ',+')) if params[:ingredients]
   end
 end
--- app/javascript/controllers/display_recipes_controller.js
+++ app/javascript/controllers/display_recipes_controller.js
@@ -1,21 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "display"]
+  static targets = ["ingredient", "input"]
 
   connect() {
     console.log("Controller connected");
   }
 
-  insertRecipes (input) {
-    this.displayTarget.classList.remove('d-none');
-    this.displayTarget.insertAdjacentHTML('beforeend', `<%= @recipes = @spoonacular.recipes(${input})%>`)
+  activate (event) {
+    event.currentTarget.classList.toggle('active');
+    // this.ingredientTarget.classList.toggle('active');
+    console.log("active");
   }
 
-  listRecipes(event) {
+  buildQuery (event) {
     event.preventDefault()
-    this.displayTarget.innerHTML = ''
-    this.displayTarget.classList.add('d-none')
-    this.insertRecipes(this.inputTarget.value.replaceAll(/[, ]+/g, ',+'))
+    let query = ""
+    this.ingredientTargets.forEach(ingredient => {
+      if (ingredient.classList.contains('active')) {
+        query += `${ingredient.innerText},+`;
+      }})
+    query = query.slice(0, -2);
+    console.log(query);
+    this.inputTarget.value = query
   }
+
+  // insertRecipes (input) {
+  //   this.displayTarget.classList.remove('d-none');
+  //   this.displayTarget.insertAdjacentHTML('beforeend', `<%= @recipes = @spoonacular.recipes(${input})%>`)
+  // }
+
+  // listRecipes(event) {
+  //   event.preventDefault()
+  //   this.displayTarget.innerHTML = ''
+  //   this.displayTarget.classList.add('d-none')
+  //   this.insertRecipes(this.inputTarget.value.replaceAll(/[, ]+/g, ',+'))
+  // }
 }
