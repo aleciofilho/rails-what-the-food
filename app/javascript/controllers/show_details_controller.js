bca330072733f58091abc885fbbb4ea452459830
gabriel tavares <gabriel29t@gmail.com>
2022-03-17 17:28:50 -0300

    Modal created

--- app/javascript/controllers/show_details_controller.js
+++ app/javascript/controllers/show_details_controller.js
@@ -1,23 +1,22 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["id", "info", "card"]
+  static targets = ["id", "serving", "minutes", "url", "summary", "likes"]
 
   connect() {
   }
 
   insertDetails(data) {
-    this.infoTarget.insertAdjacentHTML('beforeend',
-      `
-      <p>"${data.summary.substring(0, 280)}..."</p>
-      <span>Pronto em ${data.readyInMinutes} minutos</span> |
-      <span>Serve ${data.servings} pessoas</span><br>
-      <a href="${data.sourceUrl}" target="_blank">Ir para o site</a>`);
+    this.likesTarget.insertAdjacentHTML('beforeend', `${data.aggregateLikes} <i class="fas fa-thumbs-up"></i>`);
+    this.servingTarget.insertAdjacentHTML('beforeend', `Serving ${data.servings} people`);
+    this.minutesTarget.insertAdjacentHTML('beforeend',` ${data.readyInMinutes} minutes`);
+    this.summaryTarget.insertAdjacentHTML('beforeend', `"${data.summary.substring(0, 280)}..."`);
+    this.urlTarget.insertAdjacentHTML('beforeend', `<a href="${data.sourceUrl}" target="_blank">Ir para o site</a>`);
   }
 
   fetchRecipe(id) {
     console.log(id);
-    const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=eb83e47907a244ab86a9aeccc94ca035&includeNutrition=false`
+    const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=8e744ecd647445a089bee564ac8bcd73&includeNutrition=false`
     fetch(url)
       .then(response => response.json())
       .then((data) => {
@@ -27,11 +26,6 @@ export default class extends Controller {
 
   show(event) {
     event.preventDefault();
-    this.infoTarget.innerHTML = '';
-    // this.infoTarget.classList.add('d-none');
-    if (this.infoTarget.classList.contains('d-none')) {
-      this.fetchRecipe(this.idTarget.innerText);
-    }
-    this.infoTarget.classList.toggle('d-none');
+    this.fetchRecipe(this.idTarget.innerText);
   }
 }
