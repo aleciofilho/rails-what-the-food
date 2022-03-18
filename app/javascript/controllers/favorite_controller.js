3ff411075844ed5be32830f53b752b3dfd2b20a2
aleciofilho <alecio_filho@hotmail.com>
2022-03-18 17:34:10 -0300

    toggle favorite

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -20,6 +20,20 @@ export default class extends Controller {
     }
   }
 
+  toggleFavorite(event){
+    const p = event.currentTarget.parentNode;
+    event.currentTarget.classList.toggle("heart-icon-inactive")
+    if (p.classList.contains('saved')){
+      this.duplicateTarget.classList.remove('d-none');
+      setTimeout(() => { this.duplicateTarget.classList.add('d-none'); }, 3000);
+    }else {
+      this.alertTarget.classList.remove('d-none');
+      p.classList.add('saved');
+      setTimeout(() => { this.alertTarget.classList.add('d-none'); }, 3000);
+    }
+  }
+
+
   close(event){
     const p = event.currentTarget.parentNode;
     p.classList.toggle('d-none');
