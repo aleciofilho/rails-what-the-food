eddba700c07159578a90217af142c37e2e02e012
gabriel tavares <gabriel29t@gmail.com>
2022-03-20 17:12:06 -0300

    Fridge page created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: %i[show]
+  resources :users, only: %i[show] do
+    resources :fridges, only: %i[show]
+  end
 
   root to: 'pages#home'
 
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
   delete "favorites/delete/:spoon_id", to: "favorites#destroy_favorite", as: :destroy_favorite
 
   resources :recipes, only: %i[index]
-  resources :fridges, only: %i[show create destroy]
+  resources :fridges, only: %i[create destroy]
+  get 'users/:id/fridge', to: 'users#show_fridge', as: :show_fridge
   resources :favorites, only: %i[create destroy]
 end
