ea9895aaaa9d2df7984f549c30ef48a69f4712fa
Lucas Barros <lucasmabarros@gmail.com>
2022-03-21 12:26:06 -0300

    update home

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,7 +5,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Choose your Ingredients", "and find your recipes"],
+      strings: ["Find your recipes with the ingredients you have", "You can sign up for manage your fridge", "Lastly you can favorite your most loved recipes"],
       typeSpeed: 70,
       loop: true
     });
